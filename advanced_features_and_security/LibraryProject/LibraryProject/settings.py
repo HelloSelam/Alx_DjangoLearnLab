c4af09ada009d437146c616ce40f2c9d25de6785
Selamawit <selamawit8916@gmail.com>
2025-09-14 19:44:07 +0300

    fix: add SECURE_PROXY_SSL_HEADER for HTTPS proxy support

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -174,3 +174,6 @@ CSRF_COOKIE_SECURE = True
 X_FRAME_OPTIONS = "DENY"  # prevent clickjacking
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
+
+# Recognize HTTPS requests behind a proxy/load balancer
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
